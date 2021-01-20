5255a3f1c20c4092c4ec85e1940bdfe7c8211e88
vchandrakanth <48049304+vchandrakanth@users.noreply.github.com>
2021-01-20 16:35:18 +0530

    Delete build directory

--- build/conf/properties.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    qaUrl: "https://sketch.cloud.stacklynx.com/#!/",
-    seleniumServerUrl: "http://localhost:4444/wd/hub",
-    userName: "zoetisadm",
-    passWord: "zoetis@123",
-    tenant: "zoetis"
-};
