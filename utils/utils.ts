116645040c83ecdd8a707113899d52f5ee3fd243
vchandrakanth <48049304+vchandrakanth@users.noreply.github.com>
2021-01-20 17:43:53 +0530

    Update utils.ts

--- utils/utils.ts
+++ utils/utils.ts
@@ -11,6 +11,19 @@ export  async function goToMainPage( )  {
 
 export async function  getRandomNum (min, max){
     return parseInt(Math.random() * (max - min) + min);
-  };
+};
 
+public async waitUntilClicked(elem: ElementFinder): Promise<{ }> {
+    await this.waitForElementToBeClickable(elem);
+    return new Promise((resolve) => {
+      const interval = setInterval(() => {
+        elem.click().then(() => {
+          clearInterval(interval);
+          resolve();
+        }, (err) => {
+          this.log('waiting for element to be clicked...');
+        });
+      }, 500);
+    });
+  }
 
