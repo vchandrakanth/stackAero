1f75beee1d23693f115c4ca20954b12d15da4959
vchandrakanth <48049304+vchandrakanth@users.noreply.github.com>
2021-01-20 16:51:59 +0530

    Update conf.ts

--- conf/conf.ts
+++ conf/conf.ts
@@ -18,10 +18,8 @@ export let config: Config = {
     }],
   specs: ['../specs/*.js'],
 
-  // You could set no globals to true to avoid jQuery '$' and protractor '$'
-  // collisions on the global namespace.
   noGlobals: true,
-  // Disable promise manager because we are going to use async/await
+
     SELENIUM_PROMISE_MANAGER: false,
 	
   jasmineNodeOpts: {
