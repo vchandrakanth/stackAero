fe0eb8152ac160c06a2206a10d49ab354df929d2
vchandrakanth <48049304+vchandrakanth@users.noreply.github.com>
2021-01-20 16:50:31 +0530

    Update utils.ts

--- utils/utils.ts
+++ utils/utils.ts
@@ -9,28 +9,12 @@ export  async function goToMainPage( )  {
     await browser.get(prop1.qaUrl);
     await browser.sleep(10000);
     console.log(prop1.qaUrl);
-   
-
-    
+     
 }
 
 
 export async function  getRandomNum (min, max){
     return parseInt(Math.random() * (max - min) + min);
   };
-// export function writetoFile(data:any, filename:any) {
-    
-//          var stream = fs.createWriteStream(filename);
-//             stream.write(new Buffer.from(png, 'base64'));
-//             stream.end();
-//     }
 
-//    export  async function screenGrab(data:any, filename:any){
-//     browser.takeScreenshot().then(function (png)
-//     {
-//         writetoFile(png,filename)
-//     }
-
-   
-//     );
 
