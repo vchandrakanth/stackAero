5b672a6c9bcccc6d3fc9fba85e7ea0f8ce1decc3
vchandrakanth <48049304+vchandrakanth@users.noreply.github.com>
2021-01-20 17:10:49 +0530

    Update utils.ts

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,17 +1,13 @@
-
 import { browser, ExpectedConditions,element,by} from 'protractor';
 import { async } from 'q';
 let prop1 = require('../conf/properties');
-
 
 export  async function goToMainPage( )  {
     browser.ignoreSynchronization=true;
     await browser.get(prop1.qaUrl);
     await browser.sleep(10000);
-    console.log(prop1.qaUrl);
-     
+    console.log(prop1.qaUrl);    
 }
-
 
 export async function  getRandomNum (min, max){
     return parseInt(Math.random() * (max - min) + min);
